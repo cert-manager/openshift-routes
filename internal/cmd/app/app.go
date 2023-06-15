66130b29f9ccb998b3d1e40b049398f9e400c631
Alexandre <74072635+poponealex@users.noreply.github.com>
2023-06-15 15:25:40 +0200

    Revert "Try importing local dirs"
    This reverts commit 20438b013fee3cc5e4ca1861ad178780116c4dbe.
    
    Signed-off-by: Alexandre Perlmutter <alexandre.perlmutter@gmail.com>

--- internal/cmd/app/app.go
+++ internal/cmd/app/app.go
@@ -32,8 +32,8 @@ import (
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	"internal/cmd/app/options"
-	"internal/controller"
+	get "github.com/poponealex/openshift-routes/internal/cmd/app/options"
+	get "github.com/poponealex/openshift-routes/internal/controller"
 )
 
 func Command() *cobra.Command {
