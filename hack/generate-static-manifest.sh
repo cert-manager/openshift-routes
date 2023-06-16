707964f2c615028018e1513f22ff52a79fbca6c0
Alexandre Perlmutter <alexandre.perlmutter@gmail.com>
2023-06-16 13:36:45 +0200

    Merely replace `RELEASED_VERSION` env with `envsubst` in the manifest
    Signed-off-by: Alexandre Perlmutter <alexandre.perlmutter@gmail.com>

--- hack/generate-static-manifest.sh
+++ hack/generate-static-manifest.sh
@@ -5,5 +5,5 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 export RELEASED_VERSION="$1"
-envsubst < "./deploy/static/cert-manager-openshift-routes.yaml" > "cert-manager-openshift-routes-$RELEASED_VERSION.yaml"
+envsubst '${RELEASED_VERSION}' < "./deploy/static/cert-manager-openshift-routes.yaml" > "cert-manager-openshift-routes-$RELEASED_VERSION.yaml"
 exit 0
